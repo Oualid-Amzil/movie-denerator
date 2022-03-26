c31071fc6c216ba6552da63b9dce7bb66e54820a
Oualid Amzil <raskolanikouf2@gmail.com>
2022-03-26 08:29:01 +0000

    Updates

--- src/UI/BarIcon.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import MenuIcon from "@mui/icons-material/Menu";
-
-const BarIcon = () => {
-  return <MenuIcon className="menu__icon" />;
-};
-
-export default BarIcon;
